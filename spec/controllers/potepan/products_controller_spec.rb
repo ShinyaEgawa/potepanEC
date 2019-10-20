adbc5c37d48a3a3905050ac2e774ffdcfb1e9144
Egawa Shinya <sreedsmain@gmail.com>
2019-10-20 06:34:18 +0000

    Display implementation of related products

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Potepan::ProductsController, type: :controller do
   render_views
   describe 'GET #show' do
-    let!(:product) { create(:product) }
+    let!(:taxon) { create(:taxon) }
+    let!(:product) { create(:product, taxons: [taxon]) }
+    let!(:related_products) { create_list(:product, 5, taxons: [taxon]) }
 
     before do
       get :show, params: { id: product.id }
@@ -21,8 +23,12 @@ RSpec.describe Potepan::ProductsController, type: :controller do
       expect(assigns(:product)).to eq product
     end
 
-    it 'ページ名がpage_title - base_titleになっていること' do
-      assert_select "title", "#{product.name} - BIGBAG Store"
+    it "@related_productsが4つ表示されていること" do
+      expect(assigns(:related_products).count).to eq 4
+    end
+
+    it 'メインの商品が関連商品に表示されていないこと' do
+      expect(assigns(:related_products)).not_to eq include product
     end
   end
 end
