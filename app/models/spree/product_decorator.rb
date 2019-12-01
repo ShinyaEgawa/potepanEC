817164bf643734b11ee275e769f04d347eb4319e
Shinya Egawa <shin_mc5to13jp_craft@icloud.com>
2019-12-01 22:08:28 +0900

    Database modification

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -8,5 +8,5 @@ Spree::Product.class_eval do
   def related_products
     Spree::Product.in_taxons(taxons).includes(master: [:default_price, :images]).where.not(id: id).distinct # rubocop:disable Metrics/LineLength
   end
-  Spree::Product.none
+  # Spree::Product.none
 end
